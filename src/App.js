063a6245a300321d93eb83b9ef5394a8b3eb056b
ASHISH BISHT <ashish23bisht@gmail.com>
2025-05-20 23:52:16 +0530

    useState, Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'; 
-import { ReactUseEffect } from "./components/useEffects";
-import { EventHandling } from "./components/eventHandling";
-import { EventProps } from './components/eventProps';
+// import { ReactUseEffect } from "./components/useEffects";
+// import { EventHandling } from "./components/eventHandling";
+// import { EventProps } from './components/eventProps';
+import { State } from './components/hooks/useState/idex';
 
 function App() {
   return (
     <div className="App">
     {/* <EventHandling/> */}
     {/* <ReactUseEffect/> */}
-    <EventProps/>
+    {/* <EventProps/> */}
+    <State></State>
     </div>
     
   );
--- src/components/eventProps.jsx
+++ src/components/eventProps.jsx
@@ -1,35 +1,34 @@
-
-const HandelWelcomeUser = (user)=>{
-    alert(`Hello ${user} welcome!`);
-    
-};
-const handleHover = ()=>{
-    console.log("Thanks For hovering me ")
-    
-};
+//Props are arguments passed into React components.
+// Props are passed to components via HTML attributes.
 
 export const EventProps = () => {
-    return(
+  const HandelWelcomeUser = (user) => {
+    alert(`Hello ${user} welcome!`);
+  };
+  const handleHover = () => {
+    console.log("Thanks For hovering me ");
+  };
+  return (
     <>
-        <WelcomeUser 
-        onClick={()=> HandelWelcomeUser("Ashish")}
-        onMouseEnter={handleHover}/>
-    </>);
+      <WelcomeUser
+        onClick={() => HandelWelcomeUser("Ashish")}
+        onMouseEnter={handleHover}
+      />
+    </>
+  );
 };
 
 export const WelcomeUser = (props) => {
+  const HandleGreeting = () => {
+    console.log(`Hey User , Welcome!`);
+    props.onClick();
+  };
 
-    const HandleGreeting= ()=>{
-        console.log(`Hey User , Welcome!`);
-        props.onClick();
-    }
-
-    return(
+  return (
     <>
-        <button onClick={props.onClick}>Click me</button>
-        <button onMouseEnter={props.onMouseEnter}>Hover me</button>
-        <button onClick={HandleGreeting}>Greetings</button>
-    </>);
+      <button onClick={props.onClick}>Click me</button>
+      <button onMouseEnter={props.onMouseEnter}>Hover me</button>
+      <button onClick={HandleGreeting}>Greetings</button>
+    </>
+  );
 };
-
-
