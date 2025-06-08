2335a56fe7d7a16ca8b9e1163a2f4b18819f61fd
ashish23072002 <ashish23bisht@gmail.com>
2025-06-08 09:31:53 +0530

    Fetch Pokémon API: Fetching API Data in React

--- src/components/hooks/useEffects/FeatchApi.jsx
+++ src/components/hooks/useEffects/FeatchApi.jsx
@@ -1,26 +1,55 @@
 import { useEffect, useState } from "react";
+import "./Pokemon.css"
 
 export const FeatchApi = () => {
-  // const API = "https://pokeapi.co/api/v2/pokemon/pikachu";
-  const [apiData, setApiData] = useState([]);
+  const API = "https://pokeapi.co/api/v2/pokemon/squirtle";
+  const [pokemon, setPokemon] = useState(null);
 
+  const featchPokemon = () => {
+    fetch(API)
+      .then((res) => res.json())
+      .then((data) => {
+        setPokemon(data);
+      })
+      .catch((error) => console.log(error));
+  };
 
-  useEffect(()=>{
-  fetch("https://jsonplaceholder.typicode.com/posts")
-    .then((res) => res.json())
-    .then((data) => {
-        setApiData(data)  
-    })
-    .catch((error) => console.log(error));
-  },[])
+  useEffect(() => {
+    featchPokemon();
+  }, []);
 
-  return (
-    <div className="container effect-container">
-      <ul>{apiData.map((curData) => {return(
-        <li key={curData.id}>
-            {curData.title}
-        </li>
-      )})}</ul>
-    </div>
-  );
+  console.log(pokemon);
+
+  if (pokemon) {
+    return (
+      <section className="container">
+        <header>
+          <h1> Lets Catch Pokémon</h1>
+        </header>
+        <ul className="card-demo">
+          <li className="pokemon-card">
+            <figure>
+              <img
+                src={pokemon.sprites.other.dream_world.front_default}
+                alt={pokemon.name}
+                className="pokemon-image"
+              />
+            </figure>
+            <h1>{pokemon.name}</h1>
+            <div className="grid-three-cols">
+              <p className="pokemon-info">
+                Height: <span> {pokemon.height} </span>
+              </p>
+              <p className="pokemon-info">
+                Weight: <span> {pokemon.weight}</span>
+              </p>
+              <p className="pokemon-info">
+                speed: <span>{pokemon.stats[5].base_stat}</span>
+              </p>
+            </div>
+          </li>
+        </ul>
+      </section>
+    );
+  }
 };
